0b11438d4d6eca0150fd717e869ccfadd5e27d17
Yuichi Sato <y_sato@nnn.ed.jp>
2017-10-17 17:44:16 +0900

    リファクタリング

--- src/asset.js
+++ src/asset.js
@@ -1,11 +1,24 @@
 export class Asset {
+  
   constructor (assets) {
     this._assets = assets;
   }
 
+  initConstantValues () {
+  }
+
+  static get RESOURCE_TYPE_IMAGE () {
+    return "image";
+  }
+
   load () {
     for (var key of Object.keys (this._assets)) {
-      var img = loadImage(this._assets[key]);
+      let asset = this._assets[key];
+      switch (asset.type) {
+      case Asset.RESOURCE_TYPE_IMAGE:
+        asset.resource = loadImage(asset.url);
+        break;
+      }
     }
   }
 }
